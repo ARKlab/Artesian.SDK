fd7a9aeea63cf68e8c68d6409fef31eb62ac3ad6
Jacopo Cina <jacopo.cina@ark-energy.eu>
2026-05-18 10:10:24 +0200

    changed description of DerivedTransformQueryValidation.Transform

--- Artesian/Artesian.SDK/Dto/Utils/DerivedTransformQueryValidation.cs
+++ Artesian/Artesian.SDK/Dto/Utils/DerivedTransformQueryValidation.cs
@@ -19,16 +19,20 @@
             }
 
             /// <summary>
-            /// The Transform query to apply on the referenced timeseries.
-            /// The query is executed against the helper table `$table`.
-            /// For ActualTimeSerie the table exposes `Time` (datetime) and `Value` (double).
-            /// For VersionedTimeSerie it exposes `Version` (datetime), `Time` (datetime) and `Value` (double).
-            /// The transform query should return `Time` and `Value` columns in the response.
-            /// Query examples
+            /// The SQL-like transform query to apply on the referenced time series.
+            /// </summary>
+            /// <remarks>
+            /// The query is executed against the helper table <c>$table</c>.
+            /// For <b>ActualTimeSerie</b>, the table exposes <c>Time</c> (datetime) and <c>Value</c> (double).
+            /// For <b>VersionedTimeSerie</b>, it exposes <c>Version</c> (datetime), <c>Time</c> (datetime), and <c>Value</c> (double).
+            /// The transform query should return <c>Time</c> and <c>Value</c> columns in the response.
+            /// </remarks>
+            /// <example>
+            /// Example queries:
             /// SELECT Time + INTERVAL 1 DAY AS Time, Value FROM $table
             /// SELECT Time, CASE WHEN EXTRACT(HOUR FROM (Time AT TIME ZONE 'UTC') AT TIME ZONE 'Europe/Rome') &lt; 10 THEN Value + 1 ELSE Value END AS Value FROM $table WHERE Time IS NOT NULL
-            /// SELECT Time, Value FROM $table WHERE Version IS NOT NULL AND((EXTRACT(hour FROM Version) &lt; 10 AND Time &gt;= date_trunc('day', Version + interval '1 day')) OR(EXTRACT(hour FROM Version) >= 10 AND Time >= date_trunc('day', Version + interval '2 day')))
-            /// </summary>
+            /// SELECT Time, Value FROM $table WHERE Version IS NOT NULL AND ((EXTRACT(hour FROM Version) &lt; 10 AND Time &gt;= date_trunc('day', Version + interval '1 day')) OR (EXTRACT(hour FROM Version) &gt;= 10 AND Time &gt;= date_trunc('day', Version + interval '2 day')))
+            /// </example>
             public string? Transform { get; init; }
 
             /// <summary>
